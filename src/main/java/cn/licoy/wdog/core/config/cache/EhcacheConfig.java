e05ab0128f5ac1ebf1149887030fa8d7458876fa
刘传桂 <liuchuangui@nonobank.com>
2019-07-23 15:48:25 +0800

    替换redis缓存

---
+++ src/main/java/cn/licoy/wdog/core/config/cache/EhcacheConfig.java
@@ -0,0 +1,27 @@
+package cn.licoy.wdog.core.config.cache;
+
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * [类描述]
+ *
+ * @author liuchuangui
+ * @date 2019/7/23
+ */
+@Configuration
+public class EhcacheConfig {
+
+    /**
+     * 设置为共享模式
+     * @return
+     */
+    @Bean
+    public EhCacheManagerFactoryBean ehCacheManagerFactoryBean() {
+        EhCacheManagerFactoryBean cacheManagerFactoryBean = new EhCacheManagerFactoryBean();
+        cacheManagerFactoryBean.setShared(true);
+        return cacheManagerFactoryBean;
+    }
+
+}
